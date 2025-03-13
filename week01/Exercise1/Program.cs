8084887c6f11ed0af7dd2d8510f9f98a1c9289bf
Embeddet <albrightedet@gmail.com>
2025-03-13 19:05:56 +0100

    exercise 1 and 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    
+        Console.WriteLine("What is your first name?");
+        String first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        String last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+        Console.WriteLine("What is your first name?");
+        String first_name2 = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        String last_name2 = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name2}, {first_name2} {last_name2}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+       /*int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+        int F = 60;*/
+
+ Console.Write("What is your Score? ");
+        string answer = Console.ReadLine();
+        int score = int.Parse(answer);
+
+         if (score >= 90)
+        {
+            Console.WriteLine($"Your grade is A");
+            Console.WriteLine("You passed!");
+        }
+        else if (score >= 80)
+        {
+             Console.WriteLine($"Your grade is B");
+             Console.WriteLine("You passed!");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine($"Your grade is C");
+             Console.WriteLine("You passed!");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine($"Your grade is D ");
+            Console.WriteLine("Better luck next time!");
+        }
+        else if (score <= 60)
+        {
+            Console.WriteLine($"Your grade is F ");
+            Console.WriteLine("Better luck next time!");
+        }
+
+        
     }
 }
